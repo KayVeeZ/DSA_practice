9e13c50f3dbccb8465a8812f63ecc48d8e72b12d
KayVeeZ <kshitijvashisth@gmail.com>
2024-06-17 00:11:28 +0100

    learnt to perform deletion

--- 02. Operations on Arrays/02. deletion.c
+++ 02. Operations on Arrays/02. deletion.c
@@ -7,37 +7,24 @@ void display(int arr[], int n){
     }
 }
 
-int indInsertion(int arr[], int size, int element, int capacity, int index){
-    //  code for insertion
-     if(size >= capacity){
-        return -1;
-     }
-     for (int i = size-1; i >= index; i--)
+void indDeletion(int arr[], int size, int index){
+    //  code for deletion
+     
+     for (int i = index; i < size - 1; i++)
      {
-        arr[i+1] = arr[i];
+        arr[i] = arr[i+1];
      }
-     arr[index] = element;
-     return 1;
-
 }
 
-int main(){
-    // change capacity to test errors
-    int capacity = 100; 
+int main(){ 
     
     int arr[100] = {7, 8, 12, 27, 88};
     int size = 5, element = 45, index = 3;
     display(arr,size);
-    int result = indInsertion(arr, size, element, capacity, index);
+    indDeletion(arr, size, index);
     
-    if (result == 1){
-        size += 1;
-        printf("Element has been inserted! Look!!!\n");
-        display(arr,size);
-        return 0;
-    }
-    
-    else{
-        printf("Insertion failed!");
-    }
+    size -= 1;
+    printf("Element has been deleted! Look!!!\n");
+    display(arr,size);
+    return 0;
 }
