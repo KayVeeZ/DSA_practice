324e184b14daf6bb2080204e37f062772b3451f2
KayVeeZ <kshitijvashisth@gmail.com>
2024-07-16 21:59:52 +0100

    implemented stack operations using array

--- 05. Stack/01_usingArray.c
+++ 05. Stack/01_usingArray.c
@@ -31,13 +31,31 @@ int isFull(struct stack *ptr)
     return 0;
 }
 
-struct stack *createStack(int size)
+struct stack push(struct stack *s)
 {
-    struct stack *s;
-    s->size = size;
-    s->top = -1;
-    s->arr = (int *)malloc(s->size*sizeof(int));
-    return s;
+    int element = 0;
+    printf("Please enter element to be pushed: ");
+    scanf("%d", &element);
+    printf("--------------------------------------\n");
+    s->arr[s->top+1] = element;
+    s->top++;
+    // return *s;
+}
+
+struct stack pop(struct stack *s)
+{
+    int element1 = s->arr[s->top];
+    s->top--;
+    printf("Popped element is %d\n", element1);
+    // return *s;
+}
+
+struct stack display(struct stack *s)
+{
+    for (int i = s->top; i >= 0; i--)
+    {
+        printf("Element %d is %d\n", abs(i-s->top)+1,s->arr[i]);
+    }
 }
 
 int main()
@@ -47,30 +65,73 @@ int main()
     // s.top = -1;
     // s.arr = (int *)malloc(s.size*sizeof(int));
 
-    // struct stack *s;
-    // (*s).size = 80;
-    // s->top = -1;
-    // s->arr = (int *)malloc(s->size*sizeof(int));
+    struct stack *s;
+    (*s).size = 5;
+    s->top = -1;
+    s->arr = (int *)malloc(s->size*sizeof(int));
 
+    int choice = 0;
+    do{
+        printf("Enter the intended operation on stack:\n");
+        printf("--------------------------------------\n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+        printf("--------------------------------------\n");
+        printf("Your choice: ");
+        scanf("%d", &choice);
+        printf("--------------------------------------\n");
+        switch (choice)
+        {
+            case 1:
+
+            if (isFull(s) == 1)
+            {
+                printf("Error: Stack Overflow!\n");
+                break;
+            }
+
+            else
+            {            
+                push(s);
+                break;
+            }
+
+            case 2:
+            if (isEmpty(s) == 1)
+            {
+                printf("Error: Stack is Empty!\n");
+                break;
+            }
+            else
+            {
+                pop(s);
+                break;
+            } 
+
+            case 3:
+            if (isEmpty(s) == 1)
+            {
+                printf("Error: Stack is Empty!\n");
+                break;
+            }
+            else
+            {
+                display(s);
+                break;
+            }
+        }
+
+    }
+    while(choice<4);
+
+    printf("Thanks for using my program...\n -KV\n");
     // Pushing an element manually
     // s->arr[0] = 21;
     // s->top++;
-    int size = 0;
-    scanf("Enter the size of stack you want in a positive integer number: ", &size);
-    printf("\n");
-
-    struct stack *s = createStack(size);
 
 
-    // Check if stack is empty
-    if(isEmpty(s))
-    {
-        printf("The stack is empty");
-    }
-    else
-    {
-        printf("The stack is not empty.");
-    }
-    
+
     return 0;
 }
