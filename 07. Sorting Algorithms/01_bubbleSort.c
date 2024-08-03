877ade13ab1efbc520afdb0bfa8372d04fbc595a
KayVeeZ <kshitijvashisth@gmail.com>
2024-08-03 02:36:34 +0100

    implement bubble sort and made it adaptive

---
+++ 07. Sorting Algorithms/01_bubbleSort.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void printArray(int *A, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+}
+
+void bubbleSort(int *A, int n)
+{
+    int temp;
+    int isSorted = 0;
+    for (int i = 0; i < n - 1; i++) // for number of passes
+    {
+        printf("Working on pass number %d\n", i+1);
+        isSorted = 1;
+        for (int j = 0; j < n - 1 - i; j++) // for number of comparisons in each pass
+        {
+            if (A[j] > A[j + 1])
+            {
+                temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+                isSorted = 0;
+            }
+        }
+        if(isSorted)
+        {
+            return;
+        }
+    }
+}
+
+int main()
+{
+    int A[] = {12, 54, 65, 7, 23, 9};
+    // int A[] = {1, 2, 3, 4, 5, 6};
+    int n = 6;
+    printArray(A, n); // Printing the array before sorting
+    bubbleSort(A, n); // function to sort the array
+    printArray(A, n);
+    return 0;
+}
